f08b46e545f9c62287735b71ddfefb32589fc0f6
nebkat <nebkat@teamhacksung.org>
2012-03-31 10:23:57 +0200

    build in colors: host C/C++
    Change-Id: Ic415cab53a2efa104c9d4b31ddbe8c8eb74e493d
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/definitions.mk

--- core/definitions.mk
+++ core/definitions.mk
@@ -1008,7 +1008,7 @@ endef
 
 define transform-host-cpp-to-o
 @mkdir -p $(dir $@)
-@echo "host C++: $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_PFX}"host C++:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
 $(hide) $(PRIVATE_CXX) \
 	$(addprefix -I , $(PRIVATE_C_INCLUDES)) \
 	$(shell cat $(PRIVATE_IMPORT_INCLUDES)) \
@@ -1054,7 +1054,7 @@ $(hide) $(PRIVATE_CC) \
 endef
 
 define transform-host-c-to-o-no-deps
-@echo "host C: $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_PFX}"host C:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
 $(call transform-host-c-or-s-to-o-no-deps, $(PRIVATE_CFLAGS) $(PRIVATE_CONLYFLAGS) $(PRIVATE_DEBUG_CFLAGS))
 endef
 
