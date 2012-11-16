a1c8391fd797385aec181f62ee73175771fbcd9f
iceandfire <arhamjamal@gmail.com>
2012-11-17 02:28:27 +0530

    Busubox, bash, rsync, etc added.
    Change-Id: I83d8056657ee16f8e5587418288d16e1098ae137
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -44,7 +44,13 @@ PRODUCT_PACKAGES := \
     CalendarProvider \
     bluetooth-health \
     hostapd \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+	busybox \
+    rsync \
+    libncurses \
+    nano \
+    lsof \
+    bash 
 
 PRODUCT_PACKAGES += \
     audio \
